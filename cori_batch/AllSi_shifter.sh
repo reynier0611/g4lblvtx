90492151b782d6037aabca045cb1b67bc8a8cda5
Reynier Cruz Torres <reynier@gmail.com>
2020-07-21 08:21:47 -0700

    Changed output directory in AllSi_shifter from home to alice

--- cori_batch/AllSi_shifter.sh
+++ cori_batch/AllSi_shifter.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#dir="/global/project/projectdirs/alice/ftorales" #replace per user, often with ~
-#dir="~"
 dir="/global/homes/r/reynier"
-if [[ ! -e $dir/out_AllSi ]]; then
-    mkdir $dir/out_AllSi
+outdir="/project/projectdirs/alice/reynier"
+if [[ ! -e $outdir/out_AllSi ]]; then
+    mkdir $outdir/out_AllSi
 fi
 
 source $dir/Singularity/cvmfs/sphenix.sdcc.bnl.gov/x8664_sl7/opt/sphenix/core/bin/sphenix_setup.sh -n
@@ -12,6 +11,6 @@ export MYINSTALL=$dir/Singularity/install
 source $dir/Singularity/cvmfs/sphenix.sdcc.bnl.gov/x8664_sl7/opt/sphenix/core/bin/setup_local.sh $MYINSTALL
 
 cd $dir/Singularity/g4lblvtx/macros/
-root -b -q "Fun4All_G4_FastMom.C($2, \"$dir/out_AllSi/out_$3_$1\",\"$3\")"
+root -b -q "Fun4All_G4_FastMom.C($2, \"$outdir/out_AllSi/out_$3_$1\",\"$3\")"
 
-rm -rf $dir/out_AllSi/out_$3_$1_G4LBLVtx.root
+rm -rf $outdir/out_AllSi/out_$3_$1_G4LBLVtx.root
