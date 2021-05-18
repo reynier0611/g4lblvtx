2cef93d0be0bf8a00f1609cf842ca05688bab1cc
Reynier Cruz Torres <reynier@gmail.com>
2021-05-18 16:04:10 -0700

    Added missing file

---
+++ source/AllSi_vtx_serv_2lyr_Detector.h
@@ -0,0 +1,51 @@
+// Tell emacs that this is a C++ source
+//  -*- C++ -*-.
+#ifndef ALLSI_VTX_SERV_2LYR_DETECTOR_H
+#define ALLSI_VTX_SERV_2LYR_DETECTOR_H
+
+#include <g4main/PHG4Detector.h>
+#include <Geant4/G4Material.hh>
+
+#include <set>
+#include <string>  // for string
+
+class G4LogicalVolume;
+class G4VPhysicalVolume;
+class PHCompositeNode;
+class PHG4Subsystem;
+class PHParameters;
+
+class AllSi_vtx_serv_2lyr_Detector : public PHG4Detector
+{
+	public:
+		//! constructor
+		AllSi_vtx_serv_2lyr_Detector(PHG4Subsystem *subsys, PHCompositeNode *Node, PHParameters *parameters, const std::string &dnam);
+
+		//! destructor
+		virtual ~AllSi_vtx_serv_2lyr_Detector() {}
+
+		//! construct
+		void ConstructMe(G4LogicalVolume *world) override;
+
+		void Print(const std::string &what = "ALL") const override;
+
+		//!@name volume accessors
+		//@{
+		int IsInDetector(G4VPhysicalVolume *) const;
+		//@}
+
+		void SuperDetector(const std::string &name) { m_SuperDetector = name; }
+		const std::string SuperDetector() const { return m_SuperDetector; }
+		G4Material * element_material( std::string identifier );
+		void addDetectorSection( G4LogicalVolume *logicWorld , std::string name , double sign );
+
+	private:
+		PHParameters *m_Params;
+
+		// active volumes
+		std::set<G4VPhysicalVolume *> m_PhysicalVolumesSet;
+
+		std::string m_SuperDetector;
+};
+
+#endif // ALLSI_VTX_SERV_2LYR_DETECTOR_H
